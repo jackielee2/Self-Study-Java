995a094d954eca2bea9f4b413907915a30599509
jackielee <jackielee2@nate.com>
2024-11-28 09:02:35 +0900

    ..

--- _Project/chap_01/src/sector01/exam02/RunStateMent_Exam.java
+++ _Project/chap_01/src/sector01/exam02/RunStateMent_Exam.java
@@ -5,6 +5,16 @@ public class RunStateMent_Exam {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//	변수 x를 선언하고 1을 저장..
+		//	ㄴ	초기화..
+		int x = 1;
+		int y = 2;
+		int result = x + y;
+		
+		//	콘솔창에 result의 결과를
+		//	출력하는 [ System.out.println ]
+		//	메소드 호출..
+		System.out.println(result);
 	}
 
 }
