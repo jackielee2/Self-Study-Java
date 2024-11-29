a50ef07b2aa645bfcd55dbd62620effb8a1a1470
jackielee <jackielee2@nate.com>
2024-11-30 01:05:11 +0900

    ..

---
+++ _Project/chap_01/src/sector01/exam03/Var_Init_Error.java
@@ -0,0 +1,20 @@
+package sector01.exam03;
+
+public class Var_Init_Error {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//	-	한글도 변수명으로 사용이
+		//		가능하지만 권장하지는 않음..
+		//	-	변수 선언..
+		int 데이터;
+		//int 데이터 = 5;
+		
+		int 결과 = 데이터 + 10;
+		
+		System.out.println( 결과 );
+
+	}
+
+}
