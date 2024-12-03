3fb3892058716c856bf97c42ef524f82da7c9c4b
jackielee <jackielee2@nate.com>
2024-12-03 12:20:13 +0900

    ..

---
+++ _Project/chap_01/src/sector02/exam04/Char_Test.java
@@ -0,0 +1,29 @@
+package sector02.exam04;
+
+public class Char_Test {
+	
+	public static void main(String[] args)
+	{
+		//	문자를 직접 입력..
+		char c1 = 'A';
+		System.out.println(c1);
+		
+		//	10진수로 저장..
+		char c2 = 65;
+		System.out.println(c2);
+		
+		//	16진수로 저장..
+		char c3 = '\u0041';
+		System.out.println(c3);
+		
+		char d1 = '가';
+		System.out.println(d1);
+		
+		char d2 = 44032;
+		System.out.println(d2);
+		
+		char d3 = '\uac00';
+		System.out.println(d3);
+		
+	}
+}
